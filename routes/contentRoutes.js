0aba4e26be88190082ce6251792fe85c43d85494
Nehal Udyavar <nehaludyavar@gmail.com>
2019-12-28 12:53:15 +0400

    Update create content route to require login + add user info

--- routes/contentRoutes.js
+++ routes/contentRoutes.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const requireLogin = require("../middleware/requireLogin");
 
 const Content = mongoose.model("content");
 
 module.exports = app => {
   // POST request to add content to database
-  app.post("/api/content/create", async (req, res) => {
+  app.post("/api/content/create", requireLogin, async (req, res) => {
     try {
       const data = req.body;
+      // Add user id and dates to data object
+      data._user = req.user.uid;
       data.dateAdded = Date.now();
       data.lastUpdated = Date.now();
 
+      // Create Content, save to database and send back to client
       const content = new Content(data);
       await content.save();
       res.send(content);
