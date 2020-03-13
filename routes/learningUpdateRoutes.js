78c13dcd609c3b704c94920b558adcde6bdab98a
Nehal Udyavar <nehaludyavar@gmail.com>
2020-03-12 23:11:47 -0700

    Install moment + create route to fetch summary stats

--- routes/learningUpdateRoutes.js
+++ routes/learningUpdateRoutes.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const moment = require("moment");
 const requireLogin = require("../middleware/requireLogin");
 
 const LearningUpdate = mongoose.model("learning-updates");
@@ -13,6 +14,47 @@ module.exports = app => {
         res.status(500).send(error);
       } else {
         res.send(learningUpdates);
+      }
+    });
+  });
+
+  // GET request to fetch summary stats sentence for the current week
+  app.get("/api/summary-stats", requireLogin, async (req, res) => {
+    const userId = req.user.uid;
+
+    LearningUpdate.find({ _user: userId }, (error, learningUpdates) => {
+      if (error) {
+        res.status(500).send(error);
+      } else {
+        // Going to create the following example sentence that summarizes learning activity for the week:
+        // "109 pages read across 2 books this week"
+        let totalPagesRead = 0;
+        let numUniqueBooks = 0;
+        let contentIdVal = "";
+        const currentWeek = moment().week();
+
+        learningUpdates.forEach(update => {
+          // If the week number of the learning update's timestamp is the same as the week number
+          // of the current week, then add the number of pages read to the total, and also calculate the
+          // number of unique books read
+          if (moment(update.timestamp).week() === currentWeek) {
+            totalPagesRead += update.numPagesRead;
+
+            // Calculating numbere of unique books: If the contentId is not the same as the one in the
+            // previous learning update, then increment numUniqueBooks by 1.
+            if (String(update.contentId) !== contentIdVal) {
+              numUniqueBooks += 1;
+              contentIdVal = String(update.contentId);
+            }
+          }
+        });
+
+        // Sentence summarizing book stats for the week
+        const summarySentence = `${totalPagesRead} pages read across ${numUniqueBooks} ${
+          numUniqueBooks > 1 ? "books" : "book"
+        } this week.`;
+
+        res.send({ books: summarySentence });
       }
     });
   });
