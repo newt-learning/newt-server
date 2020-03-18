f1c6c2e9d2d85b3c8a5a0b5267e6b08e5241dc2a
Nehal Udyavar <nehaludyavar@gmail.com>
2020-03-17 22:50:55 -0700

    Add formatted stats functionality for "month"

--- helpers/routesHelpers.js
+++ helpers/routesHelpers.js
@@ -11,6 +11,19 @@ function initializeStatsArray(period) {
       return weekdaysShort.map(weekday => {
         return { x: weekday, y: 0 };
       });
+    case "month":
+      // Get number of days in the month
+      const daysInMonth = moment().daysInMonth();
+      // Init. array which will have all days (0, 1, ... , 28/29/30/31)
+      const allDaysInMonth = [];
+      // Populate array
+      for (i = 0; i < daysInMonth; i++) {
+        allDaysInMonth.push(i + 1);
+      }
+
+      return allDaysInMonth.map(dayNum => {
+        return { x: String(dayNum), y: 0 };
+      });
     case "year":
       const monthsShort = moment.monthsShort();
       return monthsShort.map(month => {
@@ -25,25 +38,35 @@ function initializeStatsArray(period) {
 function getFormattedStatsByPeriod(data, period) {
   const statsArray = initializeStatsArray(period);
 
-  if (period === "day") {
-    data.forEach(update => {
-      if (update.contentType === "book") {
-        statsArray[0].value += update.numPagesRead;
-      }
-    });
-  } else if (period === "week") {
-    data.forEach(update => {
-      const weekdayNumber = moment(update.timestamp).weekday();
-      statsArray[weekdayNumber].y += update.numPagesRead;
-    });
-  } else if (period === "year") {
-    data.forEach(update => {
-      const monthNumber = moment(update.timestamp).month();
-      statsArray[monthNumber].y += update.numPagesRead;
-    });
+  switch (period) {
+    case "day":
+      data.forEach(update => {
+        if (update.contentType === "book") {
+          statsArray[0].value += update.numPagesRead;
+        }
+      });
+      return statsArray;
+    case "week":
+      data.forEach(update => {
+        const weekdayNumber = moment(update.timestamp).weekday();
+        statsArray[weekdayNumber].y += update.numPagesRead;
+      });
+      return statsArray;
+    case "month":
+      data.forEach(update => {
+        const dayOfMonth = moment(update.timestamp).date();
+        statsArray[dayOfMonth - 1].y += update.numPagesRead;
+      });
+      return statsArray;
+    case "year":
+      data.forEach(update => {
+        const monthNumber = moment(update.timestamp).month();
+        statsArray[monthNumber].y += update.numPagesRead;
+      });
+      return statsArray;
+    default:
+      return statsArray;
   }
-
-  return statsArray;
 }
 
 module.exports = { getFormattedStatsByPeriod };
