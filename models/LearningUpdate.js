4be5d6bd70631d9c4a9f38aac56ec03b1694c37b
Nehal Udyavar <nehaludyavar@gmail.com>
2020-06-11 21:42:03 -0700

    Add dateAdded field to LU model + edit timestamp in route

--- models/LearningUpdate.js
+++ models/LearningUpdate.js
@@ -4,20 +4,23 @@ const { Schema } = mongoose;
 const learningUpdateSchema = new Schema({
   _user: {
     type: String,
-    ref: "User"
+    ref: "User",
   },
   contentId: {
     type: Schema.Types.ObjectId,
-    ref: "Content"
+    ref: "Content",
   },
   previousPagesRead: Number,
   updatedPagesRead: Number,
   numPagesRead: Number,
   contentType: {
     type: String,
-    enum: ["book"]
+    enum: ["book"],
   },
-  timestamp: Date
+  // The date that the reading was done, can be backdated
+  timestamp: Date,
+  // The date that this learning update was added
+  dateAdded: Date,
 });
 
 mongoose.model("learning-updates", learningUpdateSchema);
