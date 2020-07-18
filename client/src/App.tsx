6d23d4cdc41b5f98226f2dd145ffb5a2209322ae
Nehal Udyavar <nehaludyavar@gmail.com>
2020-07-18 04:00:34 -0700

    Add my rando background design svg

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { ReactComponent as BackgroundCircles } from "./assets/background-circles.svg";
+import styles from "./App.module.css";
 
 function App() {
   return (
     <div>
       <h1>Newt</h1>
+      <div className={styles.backgroundCircleContainer}>
+        <BackgroundCircles />
+      </div>
     </div>
   );
 }
