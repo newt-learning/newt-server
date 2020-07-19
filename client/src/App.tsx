4b39598bc23729643fe5cf03bc2c26b013d674f9
Nehal Udyavar <nehaludyavar@gmail.com>
2020-07-18 21:48:41 -0700

    New landing page for all screen sizes!!

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,25 @@
 import React from "react";
-import { ReactComponent as BackgroundCircles } from "./assets/background-circles.svg";
+import { ReactComponent as PhoneGraphic } from "./assets/phone-graphic.svg";
 import styles from "./App.module.css";
 
 function App() {
   return (
-    <div>
-      <h1>Newt</h1>
-      <div className={styles.backgroundCircleContainer}>
-        <BackgroundCircles />
+    <section className={styles.App}>
+      <nav className={styles.nav}>
+        <h2 className={styles.logo}>newt</h2>
+      </nav>
+      <div className={styles.mainContainer}>
+        <div className={styles.titleContainer}>
+          <h1 className={styles.title}>Organize everything you're learning</h1>
+          <h5 className={styles.subtitle}>Sign up for early access</h5>
+          <input type="email" placeholder="Email" className={styles.input} />
+        </div>
+
+        <div className={styles.phoneGraphicContainer}>
+          <PhoneGraphic />
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
 
