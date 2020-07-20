29d29307152b56e1186c4e1998fda41cb85e735a
Nehal Udyavar <nehaludyavar@gmail.com>
2020-07-20 14:52:11 -0700

    Add alt tag to images + edit manifest

--- client/src/pages/Landing/PhoneGraphic.tsx
+++ client/src/pages/Landing/PhoneGraphic.tsx
@@ -7,16 +7,21 @@ const PhoneGraphic = () => {
       <div className={styles.mockPhone} id={styles.phone1}>
         <img
           src={require("../../assets/my_library_screenshot.png")}
+          alt="Screenshot on My Library tab on the Newt mobile app"
           className={styles.screenshot}
         />
       </div>
       <div className={styles.mockPhone} id={styles.phone2}>
         <img
           src={require("../../assets/video_screen_screenshot.png")}
+          alt="Screenshot on Video screen on the Newt mobile app"
           className={styles.screenshot}
         />
       </div>
-      <img src={require("../../assets/background-circles.svg")} />
+      <img
+        src={require("../../assets/background-circles.svg")}
+        alt="A bunch of white and light-blue circles"
+      />
     </div>
   );
 };
