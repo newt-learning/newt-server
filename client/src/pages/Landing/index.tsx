812d1ac7d319f056eff2053508d3f68232cd5687
Nehal Udyavar <nehaludyavar@gmail.com>
2020-07-22 18:27:52 -0700

    Text for environment section

--- client/src/pages/Landing/index.tsx
+++ client/src/pages/Landing/index.tsx
@@ -79,6 +79,33 @@ const LandingPage = () => {
       </section>
       <section className={styles.environmentContainer}>
         <h2 className={styles.environmentTitle}>One unified environment</h2>
+        <div className={styles.environmentTextContainer}>
+          <p className={styles.environmentText}>
+            The Internet has changed the way we learn. We read books and
+            articles, watch videos, listen to podcasts, and play games, created
+            and shared by millions of incredble creators around the world. And
+            that’s awesome. But the immense scale introduces some challenges:
+          </p>
+          <ul
+            className={`${styles.environmentText} ${styles.environmentTextList}`}
+          >
+            <li>
+              it is difficult to <strong>organize</strong> <i>across</i> media
+            </li>
+            <li>
+              it is time-consuming and tricky to <strong>find</strong> the right
+              high-quality content
+            </li>
+            <li>
+              it takes extra effort to <strong>retain</strong> what you’ve
+              learned
+            </li>
+          </ul>
+          <p className={styles.environmentText}>
+            What if your learning flow could look something like this?
+          </p>
+        </div>
+
         <div className={styles.newtFlowContainer}>
           <img
             src={require("../../assets/newt-flow-diagram.svg")}
