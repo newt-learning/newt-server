1b54c8191f93d7769cb3e18fc97378e81ed9c1b3
Nehal Udyavar <nehaludyavar@gmail.com>
2020-07-27 02:26:32 -0700

    Create link in navbar to discover page

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import styles from "./Navbar.module.css";
 
 const Header = () => {
   return (
     <nav className={styles.nav}>
-      <div className={styles.logo}>newt</div>
+      <NavLink to="/" className={styles.logo}>
+        newt
+      </NavLink>
+      <div className={styles.navLinkGroup}>
+        <NavLink
+          to="/discover"
+          className={styles.navLink}
+          activeClassName={styles.activeNavLink}
+        >
+          Discover
+        </NavLink>
+      </div>
     </nav>
   );
 };
