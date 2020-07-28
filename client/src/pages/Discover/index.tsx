3ba4fd167fd69f4301ea6bd21efa37b8c7001330
Nehal Udyavar <nehaludyavar@gmail.com>
2020-07-28 03:00:28 -0700

    Init. Discover section UI layout

--- client/src/pages/Discover/index.tsx
+++ client/src/pages/Discover/index.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import { Navbar } from "../../components";
+import styles from "./Discover.module.css";
 
 const DiscoverPage = () => {
   return (
     <section>
       <Navbar />
+      <div className={styles.container}>
+        <aside className={styles.sidebar}>
+          <h2 className={styles.sidebarTitle}>Categories</h2>
+        </aside>
+        <div className={styles.mainContent}>
+          <h1 className={styles.title}>Discover</h1>
+          <p className={styles.description}>
+            Find the next thing you want to learn from our curated library.
+          </p>
+        </div>
+      </div>
     </section>
   );
 };
