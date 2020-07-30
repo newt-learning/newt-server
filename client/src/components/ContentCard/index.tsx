98fb9fe083e61257dc508d77defc36f88958d009
Nehal Udyavar <nehaludyavar@gmail.com>
2020-07-29 20:56:18 -0700

    Move Link to component + fix key warning

--- client/src/components/ContentCard/index.tsx
+++ client/src/components/ContentCard/index.tsx
@@ -1,5 +1,9 @@
 import React from "react";
+import { Link } from "react-router-dom";
+// Styling
 import styles from "./ContentCard.module.css";
+// Helpers
+import { slugify } from "../../helpers/textHelpers";
 
 export interface ContentCardProps {
   name: string;
@@ -9,11 +13,17 @@ export interface ContentCardProps {
 
 const ContentCard = ({ name, thumbnailUrl, creator }: ContentCardProps) => {
   return (
-    <div className={styles.card}>
-      <img src={thumbnailUrl} className={styles.img} />
-      <div className={styles.name}>{name}</div>
-      <div className={styles.creator}>{`by ${creator}`}</div>
-    </div>
+    <Link to={`/${slugify(creator)}/content/${slugify(name)}`}>
+      <div className={styles.card}>
+        <img
+          src={thumbnailUrl}
+          className={styles.img}
+          alt={`Thumbnail for ${name}`}
+        />
+        <div className={styles.name}>{name}</div>
+        <div className={styles.creator}>{`by ${creator}`}</div>
+      </div>
+    </Link>
   );
 };
 
--- client/src/pages/Discover/index.tsx
+++ client/src/pages/Discover/index.tsx
@@ -1,16 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
 // API
 import { useFetchNewtContent } from "../../api/newtContent";
 // Components
 import { Navbar, ContentCard } from "../../components";
 // Styling
 import styles from "./Discover.module.css";
-// Helpers
-import { slugify } from "../../helpers/textHelpers";
 
 interface DataProps {
-  id: string;
+  _id: string;
   name: string;
   thumbnailUrl: string;
   contentCreator: {
@@ -41,19 +38,18 @@ const DiscoverPage = () => {
             {status === "loading"
               ? "Loading..."
               : data.map(
-                  ({ id, name, thumbnailUrl, contentCreator }: DataProps) => (
-                    <Link
-                      to={`/${slugify(contentCreator.name)}/content/${slugify(
-                        name
-                      )}`}
-                    >
-                      <ContentCard
-                        key={id}
-                        name={name}
-                        thumbnailUrl={thumbnailUrl}
-                        creator={contentCreator.name}
-                      />
-                    </Link>
+                  ({
+                    _id: id,
+                    name,
+                    thumbnailUrl,
+                    contentCreator,
+                  }: DataProps) => (
+                    <ContentCard
+                      key={id}
+                      name={name}
+                      thumbnailUrl={thumbnailUrl}
+                      creator={contentCreator.name}
+                    />
                   )
                 )}
           </div>
