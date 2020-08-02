8cc9a0e46a5f31f7fb449da1a1ff5685038914e0
Nehal Udyavar <nehaludyavar@gmail.com>
2020-08-01 17:49:06 -0700

    Init. Quiz Intro section

--- client/src/components/QuizModal/index.tsx
+++ client/src/components/QuizModal/index.tsx
@@ -1,11 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 // Components
 import Modal from "react-bootstrap/Modal";
 import QuizModalContent from "./QuizModalContent";
 
+type Quiz = {
+  _id: string;
+  contentId: string;
+  questions: [object];
+} | null;
+
 interface QuizModalProps {
   showModal: boolean;
-  quiz: object | null;
+  quiz: Quiz;
   quizName: string;
   onCloseModal: () => void;
 }
@@ -19,12 +25,22 @@ const QuizModal = ({
   onCloseModal,
 }: QuizModalProps) => {
   const [currentSection, setCurrentSection] = useState<CurrentSection>("intro");
+  const [numQuestions, setNumQuestions] = useState(
+    quiz?.questions?.length ?? 0
+  );
+
+  useEffect(() => {
+    if (quiz) {
+      setNumQuestions(quiz.questions.length);
+    }
+  }, [quiz]);
 
   return (
     <Modal show={showModal} size="lg" backdrop="static" animation={false}>
       <QuizModalContent
         currentSection={currentSection}
         quizName={quizName}
+        numQuestions={numQuestions}
         onCloseModal={onCloseModal}
       />
     </Modal>
