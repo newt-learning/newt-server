37c851f804ca968b8f885071e06f32648a5589d1
Nehal Udyavar <nehaludyavar@gmail.com>
2020-08-05 00:06:45 -0700

    Add firebase + create Auth Context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { ToastProvider } from "react-toast-notifications";
+// Context
+import {
+  Provider as AuthProvider,
+  Context as AuthContext,
+  useData as useAuthData,
+} from "./context/AuthContext";
 // Pages
 import { LandingPage, LoginPage, DiscoverPage, ContentPage } from "./pages";
 
-function App() {
+const App = () => {
+  console.log(AuthContext);
+  const {
+    state: { isFetching, exists },
+    tryLocalSignIn,
+  } = useAuthData();
+
+  useEffect(() => {
+    tryLocalSignIn();
+  }, []);
+
+  return (
+    <Switch>
+      <Route path="/login" component={LoginPage} />
+      <Route path="/discover" component={DiscoverPage} />
+      <Route
+        path="/:creator/content/:contentNameSlug"
+        component={ContentPage}
+      />
+      <Route path="/" component={LandingPage} />
+    </Switch>
+  );
+};
+
+export default () => {
   return (
     <Router>
       <ToastProvider autoDismiss={true} placement="bottom-center">
-        <Switch>
-          <Route path="/login" component={LoginPage} />
-          <Route path="/discover" component={DiscoverPage} />
-          <Route
-            path="/:creator/content/:contentNameSlug"
-            component={ContentPage}
-          />
-          <Route path="/" component={LandingPage} />
-        </Switch>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
       </ToastProvider>
     </Router>
   );
-}
-
-export default App;
+};
