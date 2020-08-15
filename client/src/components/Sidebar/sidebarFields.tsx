81d1308f175d4248e8bd21d91b7b6b6c9cccef9f
Nehal Udyavar <nehaludyavar@gmail.com>
2020-08-14 18:07:45 -0700

    Change Topic icon, back btn color, + content padding

--- client/src/components/Sidebar/sidebarFields.tsx
+++ client/src/components/Sidebar/sidebarFields.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FiBook, FiMap, FiTrello } from "react-icons/fi";
+import { FiBook, FiArchive, FiTrello } from "react-icons/fi";
 // import { FaBrain } from "react-icons/fa";
 import styles from "./Sidebar.module.css";
 
@@ -12,6 +12,6 @@ export default [
   {
     name: "Topics",
     route: "/topics",
-    icon: () => <FiMap size={20} className={styles.icon} />,
+    icon: () => <FiArchive size={20} className={styles.icon} />,
   },
 ];
