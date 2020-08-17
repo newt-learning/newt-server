72d5ddc9e5ca9d3caba2b5ad0d1b8eac8f73ff9c
Nehal Udyavar <nehaludyavar@gmail.com>
2020-08-16 23:54:43 -0700

    Set up Profile page UI

--- client/src/pages/Profile/index.tsx
+++ client/src/pages/Profile/index.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { useData as useAuthData } from "../../context/AuthContext";
+import {
+  AppMainContainer,
+  AppHeaderContainer,
+  AppContentContainer,
+} from "../../components";
+import styles from "./Profile.module.css";
 
 const ProfilePage = () => {
-  return <section>Profile</section>;
+  const {
+    state: { userInfo },
+  } = useAuthData();
+
+  return (
+    <AppMainContainer>
+      <AppHeaderContainer>
+        <h2>Profile</h2>
+      </AppHeaderContainer>
+      <AppContentContainer>
+        <div className={styles.optionsContainer}>
+          <ul>
+            <li className={`${styles.option} ${styles.active}`}>Account</li>
+          </ul>
+        </div>
+        <div className={styles.detailsContainer}>
+          <h3>Account</h3>
+          <label>First Name</label>
+          <input value={userInfo?.firstName} />
+          <label>Last Name</label>
+          <input value={userInfo?.lastName} />
+        </div>
+      </AppContentContainer>
+    </AppMainContainer>
+  );
 };
 
 export default ProfilePage;
