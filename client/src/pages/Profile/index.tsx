a6b720deaed414a8bbd19d034ec5a1d3131ead40
Nehal Udyavar <nehaludyavar@gmail.com>
2020-09-21 00:05:23 -0700

    Use TabPane component in Profile page

--- client/src/pages/Profile/index.tsx
+++ client/src/pages/Profile/index.tsx
@@ -4,64 +4,40 @@ import {
   AppMainContainer,
   AppHeaderContainer,
   AppContentContainer,
-  SideNavItem,
-  SideNavLink,
+  TabPane,
 } from "../../components";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Nav from "react-bootstrap/Nav";
-import TabContainer from "react-bootstrap/TabContainer";
-import TabContent from "react-bootstrap/TabContent";
-import TabPane from "react-bootstrap/TabPane";
-import styles from "./Profile.module.css";
+import { TabPaneField } from "../../components/TabPane";
+
+const AccountTabPane = ({ userInfo }: { userInfo: any }) => (
+  <>
+    <h3>Account</h3>
+    <label>First Name</label>
+    <input value={userInfo?.firstName} readOnly />
+    <label>Last Name</label>
+    <input value={userInfo?.lastName} readOnly />
+  </>
+);
 
 const ProfilePage = () => {
   const {
     state: { userInfo },
   } = useAuthData();
 
+  let tabFields: TabPaneField[] = [
+    {
+      id: "account",
+      name: "Account",
+      renderTabPane: () => <AccountTabPane userInfo={userInfo} />,
+    },
+  ];
+
   return (
     <AppMainContainer>
       <AppHeaderContainer>
         <h2>Profile</h2>
       </AppHeaderContainer>
-      <AppContentContainer className={styles.contentForms}>
-        {/* <div className={styles.optionsContainer}>
-          <ul>
-            <li className={`${styles.option} ${styles.active}`}>Account</li>
-          </ul>
-        </div>
-        <div className={styles.detailsContainer}>
-          
-        </div> */}
-        <TabContainer
-          id="add-content-tabs"
-          defaultActiveKey="account"
-          transition={false}
-        >
-          <Row>
-            <Col lg={3} style={{ marginTop: "1rem" }}>
-              <Nav variant="pills" className="flex-column">
-                <Nav.Item as={SideNavItem}>
-                  <Nav.Link as={SideNavLink} eventKey="account">
-                    Account
-                  </Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Col>
-            <Col lg={9} className={styles.tabPane}>
-              <TabContent>
-                <TabPane eventKey="account">
-                  <h3>Account</h3>
-                  <label>First Name</label>
-                  <input value={userInfo?.firstName} readOnly />
-                  <label>Last Name</label>
-                  <input value={userInfo?.lastName} readOnly />
-                </TabPane>
-              </TabContent>
-            </Col>
-          </Row>
-        </TabContainer>
+      <AppContentContainer variant="tab-pane">
+        <TabPane id="profile-tabs" fields={tabFields} />
       </AppContentContainer>
     </AppMainContainer>
   );
