1a212935715775e427f8370f86a09b5bbd8e164f
Nehal Udyavar <nehaludyavar@gmail.com>
2020-09-24 22:02:10 -0700

    Init. youtube video API request

---
+++ client/src/pages/AddContent/helpers.ts
@@ -0,0 +1,11 @@
+// Youtube URL parser which only does full and short links, among others.
+// See: https://stackoverflow.com/questions/3452546/how-do-i-get-the-youtube-video-id-from-a-url
+export function validateYoutubeVideoUrl(url: string) {
+  const regex = /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#&?]*).*/;
+  const match = url.match(regex);
+
+  // Get videoId from url if it exists
+  const videoId = match && match[1].length === 11 ? match[1] : null;
+
+  return videoId;
+}
--- client/src/pages/AddContent/index.tsx
+++ client/src/pages/AddContent/index.tsx
@@ -1,23 +1,40 @@
 import React from "react";
+// API
+import { getYoutubeVideoInfo } from "../../api/youtubeApi";
+// Components
 import {
   AppMainContainer,
   AppHeaderContainer,
   AppContentContainer,
   TabPane,
 } from "../../components";
-import YoutubeForm from "./YoutubeForm";
+import YoutubeForm, { YoutubeFormValues } from "./YoutubeForm";
 // Types
 import { TabPaneField } from "../../components/TabPane";
+// Helpers
+import { validateYoutubeVideoUrl } from "./helpers";
 
 const AddContentPage = () => {
+  const handleSubmit = async (values: YoutubeFormValues) => {
+    const { videoUrl } = values;
+
+    if (videoUrl) {
+      const videoId = validateYoutubeVideoUrl(videoUrl);
+      if (videoId) {
+        const youtubeInfo = await getYoutubeVideoInfo(videoId);
+        console.log(youtubeInfo);
+      } else {
+        alert("not a youtube url");
+      }
+    }
+  };
+
   const tabFields: TabPaneField[] = [
     {
       id: "youtube",
       name: "YouTube",
       type: "nav",
-      renderTabPane: () => (
-        <YoutubeForm onSubmit={(values) => console.log(values)} />
-      ),
+      renderTabPane: () => <YoutubeForm onSubmit={handleSubmit} />,
     },
     {
       id: "books",
