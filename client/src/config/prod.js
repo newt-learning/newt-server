2249f5542ca1d4e33c0fe4da086d2275b082786c
Nehal Udyavar <nehaludyavar@gmail.com>
2020-09-29 12:57:18 -0700

    config

--- client/src/config/prod.js
+++ client/src/config/prod.js
@@ -1,4 +1,14 @@
 module.exports = {
   connectToGoogleSheetsUrl:
     "https://script.google.com/macros/s/AKfycbx-GqLhNOgUdVXUOaC4b5qM2YOE_wm_xVySkfzePR7ftCQW0n7X/exec",
+  firebaseApiKey: "AIzaSyCOMRy2GgyJWHBFYah8I6Qfm6YzGsD4hMY",
+  firebaseAuthDomain: "newt-mobile-staging.firebaseapp.com",
+  firebaseDatabaseURL: "https://newt-mobile-staging.firebaseio.com",
+  firebaseProjectId: "newt-mobile-staging",
+  firebaseStorageBucket: "newt-mobile-staging.appspot.com",
+  firebaseMessagingSenderId: "530235201688",
+  firebaseAppId: "1:530235201688:web:8cc538e737813cf24a18ed",
+  firebaseMeasurementId: "G-L4FC5RXQ9M",
+  youtubeApiKey: "AIzaSyDb0j4Clsjga241MKPJto_mzmuwcAcVTtI",
+  googleBooksApiKey: "AIzaSyAAnhzLusMT6bRsmJpHm18SDECXuwzclxw",
 };
