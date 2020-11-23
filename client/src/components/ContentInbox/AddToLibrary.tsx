10f1432e45c914bec038b524d40e9ee102e5eaf9
Nehal Udyavar <nehaludyavar@gmail.com>
2020-11-23 14:30:11 -0800

    Form UI to add Discover series to Library

--- client/src/components/ContentInbox/AddToLibrary.tsx
+++ client/src/components/ContentInbox/AddToLibrary.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+// Context
 import { useData as useAuthData } from "../../context/AuthContext";
+// Components
+import { Formik } from "formik";
+import Form from "react-bootstrap/Form";
+import { Button } from "..";
 import GoogleSignInButton from "../../pages/Login/GoogleSignInButton";
+// Styling
 import styles from "./AddToLibrary.module.css";
 
 const AddToLibrary = () => {
@@ -14,7 +20,51 @@ const AddToLibrary = () => {
   return (
     <div className={styles.container}>
       {exists ? (
-        "Add to lib form"
+        <Formik
+          initialValues={{ shelf: "Want to Learn" }}
+          onSubmit={(values) => console.log(values)}
+        >
+          {({ initialValues, handleChange, handleSubmit }) => (
+            <Form
+              noValidate
+              onSubmit={(event) => {
+                // Prevent default of adding params to url
+                event.preventDefault();
+                handleSubmit();
+              }}
+              style={{ width: "70%" }}
+            >
+              <Form.Group controlId="shelf">
+                <Form.Label>Shelf</Form.Label>
+                <Form.Control
+                  as="select"
+                  name="shelf"
+                  defaultValue={initialValues.shelf}
+                  onChange={handleChange}
+                >
+                  <option>Currently Learning</option>
+                  <option>Want to Learn</option>
+                  <option>Finished Learning</option>
+                </Form.Control>
+              </Form.Group>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  marginTop: "2rem",
+                }}
+              >
+                <Button
+                  category="success"
+                  type="submit"
+                  style={{ width: "250px" }}
+                >
+                  Add to Library
+                </Button>
+              </div>
+            </Form>
+          )}
+        </Formik>
       ) : (
         <>
           <h5>You need to sign in to add this to your Library</h5>
