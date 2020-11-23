eb8d3a72845dfc3e78155693b9f25a66450212c8
Nehal Udyavar <nehaludyavar@gmail.com>
2020-11-23 14:02:53 -0800

    Redirect to series after signing in from modal

--- client/src/components/ContentInbox/AddToLibrary.tsx
+++ client/src/components/ContentInbox/AddToLibrary.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import { useData as useAuthData } from "../../context/AuthContext";
 import GoogleSignInButton from "../../pages/Login/GoogleSignInButton";
 import styles from "./AddToLibrary.module.css";
@@ -8,6 +9,8 @@ const AddToLibrary = () => {
     state: { exists },
   } = useAuthData();
 
+  const location = useLocation();
+
   return (
     <div className={styles.container}>
       {exists ? (
@@ -15,7 +18,7 @@ const AddToLibrary = () => {
       ) : (
         <>
           <h5>You need to sign in to add this to your Library</h5>
-          <GoogleSignInButton />
+          <GoogleSignInButton redirectTo={location.pathname} />
         </>
       )}
     </div>
