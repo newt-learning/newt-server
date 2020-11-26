fe9dd9d084fb0da6142cf4f59fff6aa50eb69ea8
Nehal Udyavar <nehaludyavar@gmail.com>
2020-11-25 19:47:01 -0800

    Nullish coalescing for formatting utils

--- client/src/components/ContentInbox/utils.ts
+++ client/src/components/ContentInbox/utils.ts
@@ -3,26 +3,26 @@ import _ from "lodash";
 // Newt series ==> user series
 export function formatNewtDiscoverSeries(newtSeriesData: any) {
   let formattedSeries: any = {
-    name: newtSeriesData?.name,
-    description: newtSeriesData?.description,
-    authors: [newtSeriesData?.seriesCreator?.name],
-    thumbnailUrl: newtSeriesData?.thumbnailUrl,
-    type: newtSeriesData?.type,
-    contentType: newtSeriesData?.contentType,
+    name: newtSeriesData?.name ?? null,
+    description: newtSeriesData?.description ?? null,
+    authors: [newtSeriesData?.seriesCreator?.name] ?? null,
+    thumbnailUrl: newtSeriesData?.thumbnailUrl ?? null,
+    type: newtSeriesData?.type ?? null,
+    contentType: newtSeriesData?.contentType ?? null,
     shelf: "Want to Learn",
     playlists: [],
     startFinishDates: [],
     isFromNewtDiscover: true, // Whether the series was added from Newt Discover page
     newtInfo: {
-      newtSeriesId: newtSeriesData?._id,
-      newtCreatorIds: [newtSeriesData?.seriesCreator?._id],
+      newtSeriesId: newtSeriesData?._id ?? null,
+      newtCreatorIds: [newtSeriesData?.seriesCreator?._id] ?? null,
       newtContentIds: _.map(newtSeriesData?.content, (video) => video._id),
     },
     seriesInfo: {
-      source: newtSeriesData?.source,
-      playlistId: newtSeriesData?.sourceId,
-      title: newtSeriesData?.name,
-      description: newtSeriesData?.description,
+      source: newtSeriesData?.source ?? null,
+      playlistId: newtSeriesData?.sourceId ?? null,
+      title: newtSeriesData?.name ?? null,
+      description: newtSeriesData?.description ?? null,
       // channelId: not available
       // datePublished: N/A
       // thumbnails: N/A
@@ -32,30 +32,30 @@ export function formatNewtDiscoverSeries(newtSeriesData: any) {
   // Format videos in series
   const formattedVideos = _.map(newtSeriesData?.content, (video, index) => {
     return {
-      name: video.name,
-      description: video.description,
+      name: video.name ?? null,
+      description: video.description ?? null,
       authors: _.map(
         newtSeriesData?.contentCreators,
         (creator) => creator.name
       ),
-      thumbnailUrl: video.thumbnailUrl,
-      type: video.type,
+      thumbnailUrl: video.thumbnailUrl ?? null,
+      type: video.type ?? null,
       partOfSeries: true,
       shelf: "Want to Learn",
       startFinishDates: [],
       isFromNewtDiscover: true,
       newtInfo: {
         newtContentId: video._id,
-        newtCreatorIds: video.contentCreators,
+        newtCreatorIds: video.contentCreators, // This is what differs from formatNewtContent()
         newtSeriesId: newtSeriesData?._id,
       },
       videoInfo: {
-        source: video.source,
-        videoId: video.sourceId,
-        playlistId: newtSeriesData?.sourceId,
-        playlistPosition: index + 1,
-        title: video.name,
-        description: video.description,
+        source: video.source ?? null,
+        videoId: video.sourceId ?? null,
+        playlistId: newtSeriesData?.sourceId ?? null,
+        playlistPosition: index + 1 ?? null,
+        title: video.name ?? null,
+        description: video.description ?? null,
         // channelId: N/A
         // thumbnails: N/A
         // datePublished: N/A
@@ -84,54 +84,7 @@ export function formatNewtPlaylist(newtPlaylistData: any) {
   };
 
   const playlistContentData = _.map(newtPlaylistData?.content, (content) => {
-    let contentData: any = {
-      name: content?.name,
-      description: content?.description,
-      authors: _.map(content?.contentCreators, (creator) => creator?.name),
-      thumbnailUrl: content?.thumbnailUrl,
-      type: content?.type,
-      shelf: "Want to Learn",
-      playlists: [],
-      startFinishDates: [],
-      isFromNewtDiscover: true,
-      newtInfo: {
-        newtContentId: content._id,
-        newtCreatorIds: _.map(
-          content?.contentCreators,
-          (creator) => creator._id
-        ),
-      },
-    };
-
-    if (content?.type === "book") {
-      contentData.bookInfo = {
-        bookId: content?.sourceId,
-        title: content?.name,
-        description: content?.description,
-        authors: _.map(content?.contentCreators, (creator) => creator?.name),
-        // subtitle: N/A
-        // imageLinks: N/A
-        // industryIdentifiers: N/A
-        // pageCount: N/A,
-        // publisher: N/A
-        // datePublished: N/A
-      };
-    }
-
-    if (content?.type === "video") {
-      contentData.videoInfo = {
-        source: content?.source,
-        videoId: content?.sourceId,
-        title: content?.name,
-        description: content?.description,
-        // playlistId: N/A
-        // playlistPosition: N/A
-        // channelId: N/A,
-        // datePublished: N/A,
-        // thumbnails: N/A,
-      };
-    }
-
+    const contentData = formatNewtContent(content);
     return contentData;
   });
 
@@ -140,15 +93,19 @@ export function formatNewtPlaylist(newtPlaylistData: any) {
 
 // Format Newt Content => user content in Library
 export function formatNewtContent(newtContentData: any) {
+  if (!newtContentData) {
+    return null;
+  }
+
   let contentData: any = {
-    name: newtContentData?.name,
-    description: newtContentData?.description,
+    name: newtContentData?.name ?? null,
+    description: newtContentData?.description ?? null,
     authors: _.map(
       newtContentData?.contentCreators,
       (creator) => creator?.name
     ),
-    thumbnailUrl: newtContentData?.thumbnailUrl,
-    type: newtContentData?.type,
+    thumbnailUrl: newtContentData?.thumbnailUrl ?? null,
+    type: newtContentData?.type ?? null,
     shelf: "Want to Learn",
     playlists: [],
     startFinishDates: [],
@@ -168,9 +125,9 @@ export function formatNewtContent(newtContentData: any) {
 
   if (newtContentData?.type === "book") {
     contentData.bookInfo = {
-      bookId: newtContentData?.sourceId,
-      title: newtContentData?.name,
-      description: newtContentData?.description,
+      bookId: newtContentData?.sourceId ?? null,
+      title: newtContentData?.name ?? null,
+      description: newtContentData?.description ?? null,
       authors: _.map(
         newtContentData?.contentCreators,
         (creator) => creator?.name
@@ -186,10 +143,10 @@ export function formatNewtContent(newtContentData: any) {
 
   if (newtContentData?.type === "video") {
     contentData.videoInfo = {
-      source: newtContentData?.source,
-      videoId: newtContentData?.sourceId,
-      title: newtContentData?.name,
-      description: newtContentData?.description,
+      source: newtContentData?.source ?? null,
+      videoId: newtContentData?.sourceId ?? null,
+      title: newtContentData?.name ?? null,
+      description: newtContentData?.description ?? null,
       // playlistId: N/A
       // playlistPosition: N/A
       // channelId: N/A,
